96b620415f8370c2b6eacf5c2e66de0b997f2f36
zhuchunyu <zhuchunyu@chinawatermeter.com>
2023-05-29 13:38:22 +0800

    fix: 修复 lrucache export 错误的问题

--- src/adapter/index.ts
+++ src/adapter/index.ts
@@ -2,14 +2,15 @@ import axios from 'axios';
 import retryAdapter from './retryAdapter';
 import cacheAdapter from './cacheAdapter';
 
-const axiosAdapter = () => retryAdapter(
-  cacheAdapter(axios.defaults.adapter!, {
-    enabledByDefault: false,
-  }),
-  {
-    times: 1,
-    delay: 500,
-  }
-);
+const axiosAdapter = () =>
+  retryAdapter(
+    cacheAdapter(axios.defaults.adapter!, {
+      enabledByDefault: false,
+    }),
+    {
+      times: 1,
+      delay: 500,
+    }
+  );
 
 export default axiosAdapter;
